7584311d7b44eed4f584fffd7efb72de4c6094bc
techwolf <fawasrufai999@gmail.com>
2024-01-03 00:38:39 +0100

    Techwolf001

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5000,
-  }
-})
+  server: {
+    port: 5000,
+  },
+  base: "https://techwolf001.github.io/Lauren/",
+});
